e5ac1421b85417f2745f6bdfd262b239fcf864b4
mikilukasik <szarhazi@gmail.com>
2023-09-24 21:55:49 +0200

    copy old python server

---
+++ src/server/server.py
@@ -0,0 +1,77 @@
+from flask import Flask, jsonify, request
+import numpy as np
+import threading
+import os
+from utils.load_model import load_model
+from utils.get_all_model_names import get_all_model_names
+from utils.print_large import print_large
+
+app = Flask(__name__)
+
+models_folder = '../../data/models'
+model_cache = {}
+model_timers = {}
+
+
+def clear_model_cache(model_name):
+    print('Cleaning up after model:', model_name)
+    model_cache.pop(model_name, None)
+    model_timers.pop(model_name, None)
+
+
+def replace_slash_and_dot(text):
+    text = text.replace("-slash-", "/")
+    text = text.replace("-dot-", ".")
+    return text
+
+
+@app.route('/predict/<model_name>', methods=['GET'])
+def predict(model_name):
+
+    print('predicting with model: ', model_name)
+
+    # Get the input from the request parameters
+    input_data = request.args.get('input')
+    input_data = np.array(input_data.split(','), dtype=np.float32)
+
+    # Reshape the input data
+    input_data = input_data.reshape((1, 8, 8, 14))
+
+    # Load the model from the cache or from disk
+    model = model_cache.get(model_name)
+    if model is None:
+        model_path = os.path.join(
+            models_folder, replace_slash_and_dot(model_name))
+        print('Loading model:', model_name)
+        model = load_model(model_path)
+        model_cache[model_name] = model
+        print('Loaded model:', model_name)
+
+    # Make a prediction with the model
+    prediction = model.predict(input_data)
+
+    # Convert the prediction to a string
+    prediction_str = ','.join(str(x) for x in prediction[0])
+
+    # Set a timer to clear the model from the cache after a minute
+    if model_name in model_timers:
+        model_timers[model_name].cancel()
+    timer = threading.Timer(60, clear_model_cache, args=[model_name])
+    model_timers[model_name] = timer
+    timer.start()
+
+    # Return the prediction as a JSON object
+    return jsonify({'prediction': prediction_str})
+
+
+@app.route('/models', methods=['GET'])
+def get_model_names():
+    model_names = get_all_model_names(models_folder)
+
+    # Convert the string array to a JSON object
+    return jsonify({'models': model_names})
+
+
+if __name__ == '__main__':
+    print(get_all_model_names(models_folder))
+    app.run(port=3600, debug=True, use_reloader=False)
---
+++ src/utils/load_model.py
@@ -0,0 +1,69 @@
+from typing import Union, Dict
+import os
+import json
+import tensorflow as tf
+from keras.models import model_from_json
+from .print_large import print_large
+
+
+def load_model(model_source: str) -> Union[tf.keras.Model, None]:
+    """
+    Load a Keras model from disk.
+
+    Parameters:
+        model_source (str): The path to the saved model directory.
+
+    Returns:
+        tf.keras.Model or None: The loaded Keras model, or None if an error occurred.
+    """
+
+    try:
+        model_source = os.path.abspath(
+            model_source)
+        from_json = os.path.exists(os.path.join(model_source, "model.json"))
+        if from_json:
+            with open(os.path.join(model_source, "model.json"), "r") as f:
+                model_json = f.read()
+
+            model = model_from_json(model_json)
+            try:
+                model.load_weights(os.path.join(model_source, "weights.h5"))
+                print_large('Weights loaded.', model_source)
+            except Exception as e:
+                print(f"Error loading weights: {str(e)}")
+            print_large('JSON model loaded.', model_source)
+        else:
+            model = tf.keras.models.load_model(model_source)
+            print_large('Keras model loaded.', model_source)
+        return model
+    except Exception as e:
+        print(f"Error loading model: {str(e)}")
+        return None
+
+
+def load_model_meta(model_source: str) -> Union[Dict, None]:
+    """
+    Load a metadata.json file from the given model directory.
+
+    Parameters:
+        model_source (str): The path to the saved model directory.
+
+    Returns:
+        dict or None: The loaded metadata as a dictionary, or None if an error occurred.
+    """
+
+    try:
+        model_source = os.path.abspath(
+            model_source)  # convert to absolute path
+        meta_path = os.path.join(model_source, "metadata.json")
+        if os.path.exists(meta_path):
+            with open(meta_path, "r") as f:
+                metadata = json.load(f)
+                print_large('Metadata loaded.', model_source)
+            return metadata
+        else:
+            print(f"Metadata file not found in {model_source}")
+            return {}
+    except Exception as e:
+        print(f"Error loading metadata: {str(e)}")
+        return None
